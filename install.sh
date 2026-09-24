62574c81253ffb21f408821b22c0d81ed609008d
liming <827182486@qq.com>
2026-09-25 00:06:05 +0800

    读取文本第一行

--- install.sh
+++ install.sh
@@ -189,7 +189,7 @@ tgsbshow() {
 	echo '#!/bin/bash
 export LANG=en_US.UTF-8
 
-m1=$(cat ~/_vless_reality_url_ 2>/dev/null)
+m1=$(head -n 1 ~/_vless_reality_url_ 2>/dev/null)
 message_text_m1=$(echo "$m1")
 MODE=HTML
 URL="https://api.telegram.org/bottelegram_token/sendMessage"
